baf9fb0376820d4efcefbd999a4bf2028b4dcba8
Vikrant.Corjuenkar.26073 <vcorjuenkar@salesforce.com>
2026-03-01 15:14:27 +0530

    checkkk

--- force-app/main/default/lwc/applicationDocumentSummaryNano/applicationDocumentSummaryNano.js
+++ force-app/main/default/lwc/applicationDocumentSummaryNano/applicationDocumentSummaryNano.js
@@ -613,7 +613,12 @@ export default class ApplicationDocumentSummaryNano extends LightningElement {
         return (val != '' && val != null && val != undefined) ;
     }
 
-
+    // Temporary method to validate line-level ESLint blocking in pre-commit hook.
+    eslintScannerLineLevelTest() {
+        setTimeout(() => console.log('eslint test line'), 0);
+        this.template.querySelector('div').innerHTML = 'eslint test';
+        this.template.querySelector('span').innerHTML = 'eslint test second change';
+    }
 
     precommitLineLevelCleanTest() {
         const label = 'precommit-clean-change';
