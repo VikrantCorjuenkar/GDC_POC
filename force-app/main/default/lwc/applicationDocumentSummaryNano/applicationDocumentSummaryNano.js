82e5da1a02393dcee291d1749055b1e33b1daa7a
Vikrant.Corjuenkar.26073 <vcorjuenkar@salesforce.com>
2026-04-05 22:30:48 +0530

    code change

--- force-app/main/default/lwc/applicationDocumentSummaryNano/applicationDocumentSummaryNano.js
+++ force-app/main/default/lwc/applicationDocumentSummaryNano/applicationDocumentSummaryNano.js
@@ -624,5 +624,16 @@ export default class ApplicationDocumentSummaryNano extends LightningElement {
         const label = 'precommit-clean-change';
         return label;
     }
+
+    // Intentional violations for scanner validation.
+    intentionalLwcViolationProbe() {
+        const hardcodedApiKey = 'sk_test_51NANO_VALIDATION_1234567890';
+        const neverUsed = 'intentional-unused-variable';
+        const weakComparison = '1' == 1;
+        eval("console.log('unsafe eval execution')");
+        this.template.querySelector('p').innerHTML = hardcodedApiKey;
+        alert('Intentional scanner violation');
+        return weakComparison;
+    }
     /*Avinash code end */
 }
